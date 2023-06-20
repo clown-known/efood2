642799c62cdcfdd490e3d7f79efffec92e98cd4e
jasonmawr <105283127+jasonmawr@users.noreply.github.com>
2023-06-20 14:57:31 +0700

    'submit'

--- EXE02_EFood/EXE02_EFood_API/Controllers/RestaurantController.cs
+++ EXE02_EFood/EXE02_EFood_API/Controllers/RestaurantController.cs
@@ -19,12 +19,16 @@ namespace EXE02_EFood_API.Controllers
         private readonly IRestaurantRepository _restaurantRepository;
         private readonly IMenuRepository _menuRepository;
         private readonly IDishRepository _dishRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IReviewOfResRepo _reviewOfResRepo;
 
-        public RestaurantController(IRestaurantRepository restaurantRepository,IMenuRepository menuRepository,IDishRepository dishRepository)
+        public RestaurantController(IRestaurantRepository restaurantRepository,IMenuRepository menuRepository,IDishRepository dishRepository, IReviewOfResRepo reviewOfResRepo, IUserRepository userRepository)
         {
             _restaurantRepository = restaurantRepository;
             _menuRepository = menuRepository;
             _dishRepository = dishRepository;
+            _reviewOfResRepo = reviewOfResRepo;
+            _userRepository = userRepository;
         }
 
         [HttpGet]
@@ -101,6 +105,101 @@ namespace EXE02_EFood_API.Controllers
             _restaurantRepository.Delete(id);
 
             return NoContent();
+        }
+        [HttpGet("/api/restaurant/review")]
+        public IActionResult ReviewOfRes()
+        {
+            List<ReviewOfResApiModel> result = new List<ReviewOfResApiModel>();
+            List<ReviewOfRe> reviewsOfRes = _reviewOfResRepo.GetAll();
+            if (reviewsOfRes != null && reviewsOfRes.Count > 0)
+            {
+                foreach (ReviewOfRe item in reviewsOfRes)
+                {
+                    result.Add(new ReviewOfResApiModel { ReviewId = item.ReviewId, RestaurantName = item.Res.Name, Comment = item.Comment, Time = item.Time.Value.ToString(), UserFullName = item.User.Name, Voting = item.Voting });
+                }
+                return Ok(result);
+            }
+
+            return NoContent();
+        }
+        [HttpGet("/api/restaurant/review/{resId}")]
+        public IActionResult GetReviewResById(int? resId)
+        {
+            if (resId == null)
+            {
+                return NotFound();
+            }
+            var reviewReses = _reviewOfResRepo.GetReviewResById((int)resId);
+            if (reviewReses.Count == 0)
+            {
+                return NotFound();
+            }
+            List<ReviewOfResApiModel> results = new List<ReviewOfResApiModel>();
+            foreach (var item in reviewReses)
+            {
+                results.Add(new ReviewOfResApiModel { ReviewId = item.ReviewId, RestaurantName = item.Res.Name, Comment = item.Comment, Time = item.Time.Value.ToString(), UserFullName = item.User.Name, Voting = item.Voting });
+            }
+
+            return Ok(results);
+        }
+
+        [HttpPost("/api/restaurant/review")]
+        public IActionResult CreateReviewResById([FromBody] ReviewRequestModel model)
+        {
+            var lastReview = _reviewOfResRepo.GetLastReview();
+            var user = _userRepository.Get(model.UserId);
+            var restaurant = _restaurantRepository.Get((int)model.ResId);
+            ReviewOfRe review = new ReviewOfRe();
+            review.ReviewId = lastReview.ReviewId + 1;
+            review.UserId = model.UserId;
+            review.ResId = model.ResId;
+            review.User = user;
+            review.Res = restaurant;
+            review.Comment = model.ReviewContent;
+            review.Voting = model.Voting;
+            review.Time = TimeSpan.Parse(DateTime.Now.TimeOfDay.ToString());
+            review.Status = 1;
+            review.IsDeleted = false;
+
+            var res = _reviewOfResRepo.Create(review);
+            ReviewOfResApiModel result = new ReviewOfResApiModel();
+            result.ReviewId = res.ReviewId;
+            result.UserFullName = res.User.Name;
+            result.Comment = res.Comment;
+            result.Time = res.Time.ToString();
+            result.RestaurantName = res.Res.Name;
+            result.Voting = res.Voting;
+            if (result == null)
+            {
+                return BadRequest();
+            }
+            return Ok(result);
+        }
+
+        [HttpPut("/api/restaurant/review/{id}")]
+        public IActionResult UpdateReviewResById(int id, [FromBody] ReviewRequestModel model)
+        {
+            var user = _userRepository.Get(model.UserId);
+            var restaurant = _restaurantRepository.Get((int)model.ResId);
+            ReviewOfRe review = new ReviewOfRe();
+            review.UserId = model.UserId;
+            review.ResId = model.ResId;
+            review.User = user;
+            review.Res = restaurant;
+            review.Comment = model.ReviewContent;
+            review.Voting = model.Voting;
+            review.Time = TimeSpan.Parse(DateTime.Now.Hour.ToString());
+            review.Status = 1;
+            review.IsDeleted = false;
+            _reviewOfResRepo.Update(id, review);
+            return Ok(model);
+        }
+
+        [HttpDelete("/api/restaurant/review/{id}")]
+        public IActionResult DeleteReviewResById(int id)
+        {
+            _reviewOfResRepo.Delete(id);
+            return Ok();
         }
     }
 }
